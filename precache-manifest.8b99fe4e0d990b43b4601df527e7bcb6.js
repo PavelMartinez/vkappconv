8d2ca3972396fa3458c89dcd7ae45f28f1aef62a
PavelMartinez <golovip2014@mail.ru>
2019-11-02 21:10:43 +0300

    Updates

---
+++ precache-manifest.8b99fe4e0d990b43b4601df527e7bcb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkappconv/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "7a84cef22c5535307bda",
+    "url": "/vkappconv/static/js/runtime~main.7a84cef2.js"
+  },
+  {
+    "revision": "585d11504e896cfa410f",
+    "url": "/vkappconv/static/js/main.585d1150.chunk.js"
+  },
+  {
+    "revision": "e90f909421b6db09e878",
+    "url": "/vkappconv/static/js/2.e90f9094.chunk.js"
+  },
+  {
+    "revision": "585d11504e896cfa410f",
+    "url": "/vkappconv/static/css/main.ae35e3ce.chunk.css"
+  },
+  {
+    "revision": "e90f909421b6db09e878",
+    "url": "/vkappconv/static/css/2.f17bc2cf.chunk.css"
+  },
+  {
+    "revision": "56bfc96829f20d84a2e2a827f8e264a7",
+    "url": "/vkappconv/index.html"
+  }
+];
